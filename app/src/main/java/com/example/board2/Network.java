4d02e51bf1a083e5542e2c951c39c5dfd4a96815
Administrator <khjung0925@naver.com>
2021-11-04 16:42:43 +0900

    1104

---
+++ app/src/main/java/com/example/board2/Network.java
@@ -0,0 +1,22 @@
+package com.example.board2;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Network {
+
+    private static Retrofit retrofit;
+    private static BoardService service;
+
+    public static BoardService getService() {
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://192.168.2.22:8090/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            service = retrofit.create(BoardService.class);
+        }
+        return service;
+    }
+
+}
